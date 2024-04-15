871845692c00081c2ea26f5ca9786aaf910e7a69
Yogesh Khere <ykhere14@gmail.com>
2024-04-15 20:50:17 +0530

    Updated auth guard setting

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from '../_helpers/auth.guard';
 
 const routes: Routes = [
   { path: "", component: DashboardComponent ,
 children:[
   {
-    path: "", component: HomeComponent
+    path: "", component: HomeComponent,
+    canActivate: [AuthGuard],
   }
 ]},
 ];
